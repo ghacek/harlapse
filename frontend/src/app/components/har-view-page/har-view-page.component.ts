9c492f114ad184b8b9134774c1b19ceddd3010c7
Goran Hacek <goran.hacek@gmail.com>
2023-02-23 12:37:23 +0100

    Working on displaing page info when multiple pages in HAR. Working on waterfall offset and relative timing

--- frontend/src/app/components/har-view-page/har-view-page.component.ts
+++ frontend/src/app/components/har-view-page/har-view-page.component.ts
@@ -13,12 +13,25 @@ export class HarViewPageComponent {
 
     har?: HarLog;
 
+    /** Defines if loaded HAR file contains requests from multiple pages. */
+    multiplePages = false;
+
+    /** Start time of the first page in loaded HAR file */
+    startTime?: Date;
+
+    /** Time of last request + its execution time. */
+    endTimeMilis: number = 0;
+
+    /** Difference between startTime and endTimeMilis in milliseconds. */
+    duration: number = 0;
+
     constructor(private http: HttpClient, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
         this.route.queryParamMap.subscribe(params => {
           const id = params.get('id');
+          
 
           if (id) {
             this.loadHar(id);
@@ -34,7 +47,74 @@ export class HarViewPageComponent {
     private loadHar(id: string) {
         this.id = id;
         this.http.get<HarLog>("http://localhost:8080/api/har?id=" + id)
-            .subscribe(x => this.har = x);
+            .subscribe(har => {
+                this.setHar(har);
+            });
+    }
+
+    private setHar(har: HarLog) {
+        this.har = har;
+        this.multiplePages = !!(har && har.pages && har.pages.length > 1);
+
+        return this.findFirstStartDate(har)
+            && this.setEntryOffsetTimes(har);
+    }
+
+    private findFirstStartDate(har: HarLog) {
+        const pages = har.pages || [];
+
+        for(const page of pages) {
+            const startDate = new Date(page.startedDateTime);
+            const startMilis = startDate.getTime();
+
+            if (isNaN(startMilis)) {
+                console.error("Page has invalid startedDateTime. Page id: " + page.id);
+                return false;
+            }
+
+            page['_startDate'] = startDate;
+
+   
+        }
+
+        // sort pages by startedDateTime
+        pages.sort((a, b) => (<Date>a['_startDate']).getTime() - (<Date>b['_startDate']).getTime());
+
+        return true;
+    }
+
+    private setEntryOffsetTimes(har: HarLog) {
+        for (const entry of har.entries) {
+            const startDate = new Date(entry.startedDateTime);
+            const startMilis = startDate.getTime();
+
+            const endMilis = startMilis + entry.time;
+
+            if (isNaN(startMilis)) {
+                console.error("Entry has invalid startedDateTime. ", entry);
+                return false;
+            }
+
+            entry['_startDate'] = startDate;
+
+            if (!this.startTime || startMilis < this.startTime.getTime()) {
+                this.startTime = startDate;
+            }
+            if (!this.endTimeMilis  || endMilis > this.endTimeMilis) {
+                this.endTimeMilis = endMilis;
+            }
+        }
+
+        this.duration = this.endTimeMilis! - this.startTime!.getTime();
+
+        for (const entry of har.entries) { 
+            entry['_offsetTime'] = (<Date>entry['_startDate']).getTime() - this.startTime!.getTime();
+        }
+
+        // sort pages by startedDateTime
+        har.entries.sort((a, b) => (<Date>a['_startDate']).getTime() - (<Date>b['_startDate']).getTime());
+
+        return true;
     }
 
     log(a: any) {
@@ -46,8 +126,7 @@ export class HarViewPageComponent {
             return 0;
         }
 
-        console.log("aa", value, (value / 30 * 100).toFixed(2));
-
-        return (value / 30 * 100).toFixed(2);
+        return (value / this.duration! * 100).toFixed(2);
     }
+
 }
