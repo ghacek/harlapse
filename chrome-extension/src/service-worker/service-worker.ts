b7abdd73211d5f24992d6b20180c3da6c2571960
Goran Hacek <goran.hacek@gmail.com>
2023-07-11 11:13:30 +0200

    Addded keep alive mechanisam for background service worker

--- chrome-extension/src/service-worker/service-worker.ts
+++ chrome-extension/src/service-worker/service-worker.ts
@@ -1,11 +1,27 @@
-'use strict';
-
 import { initNetworkMonitor } from "../network/monitor";
+import { createLokiLogger } from "../util/grafana-loki-logging";
 import { registerBgCommandHandler } from "./command-handling";
 
+const logger = createLokiLogger("root");
 
-console.log("Harlapse - background", chrome.runtime.getManifest().version)
+logger("Harlapse - background", chrome.runtime.getManifest().version);
+
+
 
 
 initNetworkMonitor();
 registerBgCommandHandler();
+
+chrome.runtime.onSuspend.addListener(
+    () => logger("Suspending")
+);
+
+
+// Making sure that the background script is always running.
+// Suspension timeout is delayed by calling chrome API every 20 seconds.
+// We need to keep service worker alive to preserve the state of logged network requests.
+// https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension
+// https://developer.chrome.com/docs/extensions/migrating/known-issues/
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
